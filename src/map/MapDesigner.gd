7ec6c56eb5297cb305a24635ec06c72aad3f267e
martin_cerdeira <Martin.Cerdeira@comafi.com.ar>
2023-07-14 16:37:20 -0300

    Mas cosis

--- src/map/MapDesigner.gd
+++ src/map/MapDesigner.gd
@@ -1,4 +1,14 @@
 extends Node2D
+export var _MiniMap: NodePath
+onready var MiniMap : Node2D = get_node(_MiniMap) as Node2D
+
+func _physics_process(delta):
+	var call_map = Input.is_action_just_pressed("call_map")
+	if(call_map):
+		position_coso()
+		MiniMap.visible = !MiniMap.visible
+		visible = !visible
+		get_tree().paused = visible
 
 func position_coso():
 	var pos_string = "Map" + str(Global.GLOBAL_POS.x) + str(Global.GLOBAL_POS.y)
